8c1ef16dbd3c756e39b89a2dc8a8260d5ab908ed
ehdtn0321 <ehdtn0321@naver.com>
2024-06-04 03:58:19 +0900

    MainComponent 불필요 코드 제거

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,70 +2,24 @@ import styled from 'styled-components';
 import Image from 'next/image';
 
 import MainIPhone from '@/assets/main_iphone.png';
-import SamplePhone1 from '@/assets/app_sample_1.png';
-import SamplePhone2 from '@/assets/app_sample_2.png';
-import SamplePhone3 from '@/assets/app_sample_3.png';
-import SamplePhone4 from '@/assets/app_sample_4.png';
-import SamplePhone5 from '@/assets/app_sample_5.png';
-import SamplePhone6 from '@/assets/app_sample_6.png';
 
 import Clock from '@/assets/clock.png';
 import MegaPhone from '@/assets/megaPhone.png';
-import Target from '@/assets/target.png';
+// import MainVideo from '@/videos/mainVideo.mp4';
 
 export const MainComponent = () => {
   return (
     <MainWrap>
-      {/* <Image
-        src={SamplePhone1}
-        alt="samplePhone1"
-        // width={244}
-        style={{
-          transform: 'rotate(-30deg)',
-          borderRadius: '30px',
-          position: 'absolute',
-        }}
-      /> */}
-      {/* <Image
-        src={SamplePhone2}
-        alt="samplePhone2"
-        style={{
-          transform: 'rotate(15deg)',
-          // position: 'absolute',
-        }}
-      /> */}
-      {/* <Image
-        src={SamplePhone3}
-        alt="samplePhone3"
-        style={{
-          transform: 'rotate(-15deg)',
-          // position: 'absolute',
-        }}
-      /> */}
-      {/* <Image
-        src={SamplePhone4}
-        alt="samplePhone4"
-        style={{
-          transform: 'rotate(15deg)',
-          // position: 'absolute',
-        }}
-      /> */}
-      {/* <Image
-        src={SamplePhone5}
-        alt="samplePhone5"
-        style={{
-          transform: 'rotate(-15deg)',
-          // position: 'absolute',
-        }}
-      /> */}
-      {/* <Image
-        src={SamplePhone6}
-        alt="samplePhone6"
-        style={{
-          transform: 'rotate(30deg)',
-          // position: 'absolute',
-        }}
-      /> */}
+      <video
+        width="100%"
+        muted
+        autoPlay
+        controls
+        preload="none"
+        // src={MainVideo}
+      >
+        <source type="video/mp4" />
+      </video>
 
       <Image
         className="icon-motion clock"
@@ -104,13 +58,7 @@ export const MainComponent = () => {
           <p className="text1 ">굿피티</p>
           <p className="text2">와 함께,</p>
         </div>
-        <Image
-          src={MainIPhone}
-          alt="iphone"
-          width={396}
-          height={802}
-          // style={{ position: 'absolute' }}
-        />
+        <Image src={MainIPhone} alt="iphone" width={396} height={802} />
         <p className="text2 ">발표 능력 UP!</p>
       </div>
     </MainWrap>
