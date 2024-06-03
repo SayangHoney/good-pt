bdb9d3871480800b48fbf12dc91318ca1f506f86
ehdtn0321 <ehdtn0321@naver.com>
2024-06-03 19:15:18 +0900

    main css 여백 처리
    global css 제거 및 styled component 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
 'use client';
 
 import { MainComponent } from '@/components/main';
-import styles from './page.module.css';
 import { TopComponent } from '@/components/Top';
 import { MainPr } from '@/components/mainPr';
 import { Overview } from '@/components/overview';
 import { Services } from '@/components/services';
 import { Intorduce } from '@/components/introduce';
+import styled from 'styled-components';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <MainWrap>
       <TopComponent />
       <MainComponent />
       <MainPr />
       <Overview />
       <Services />
       <Intorduce />
-    </main>
+    </MainWrap>
   );
 }
+
+const MainWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+
+  background: black;
+`;
